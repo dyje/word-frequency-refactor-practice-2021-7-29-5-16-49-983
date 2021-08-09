795418dbf262dd840130104e174518104463135e
dyje <dyje@oocl.com>
2021-08-10 00:15:44 +0800

    [WordFrequencyGame][Jesse] refactor: removed the else statement

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,37 +9,36 @@ public class WordFrequencyGame {
     public static final String WHITE_SPACE = "\\s+";
     public static final int DEFAULT_WORD_COUNT = 1;
 
-    public String getResult(String wholeSentence){
+    public String getResult(String wholeSentence) {
 
 
         if (isSingleWord(wholeSentence)) {
             return wholeSentence + " 1";
-        } else {
-
-            try {
-                List<WordInfo> wordInfoList = getWordInfos(wholeSentence);
-
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo w : wordInfoList) {
-                    String s = w.getWord() + " " +w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-
-
-                return "Calculate Error";
-            }
         }
+        try {
+            List<WordInfo> wordInfoList = getWordInfos(wholeSentence);
+
+            wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+
+            StringJoiner joiner = new StringJoiner("\n");
+            for (WordInfo w : wordInfoList) {
+                String s = w.getWord() + " " + w.getWordCount();
+                joiner.add(s);
+            }
+            return joiner.toString();
+        } catch (Exception e) {
+
+
+            return "Calculate Error";
+        }
+        
     }
 
-    private boolean isSingleWord (String wholeSentence){
+    private boolean isSingleWord(String wholeSentence) {
         return wholeSentence.split(WHITE_SPACE).length == DEFAULT_WORD_COUNT;
     }
 
-    private List<WordInfo> getWordInfos (String wholeSentence){
+    private List<WordInfo> getWordInfos(String wholeSentence) {
         //split the input string with 1 to n pieces of spaces
         String[] words = wholeSentence.split(WHITE_SPACE);
 
@@ -50,10 +49,10 @@ public class WordFrequencyGame {
         }
 
         //get the map for the next step of sizing the same word
-        Map<String, List<WordInfo>> map =getListMap(wordInfoList);
+        Map<String, List<WordInfo>> map = getListMap(wordInfoList);
 
         List<WordInfo> list = new ArrayList<>();
-        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
+        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
             WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
             list.add(wordInfo);
         }
@@ -61,17 +60,15 @@ public class WordFrequencyGame {
         return wordInfoList;
     }
 
-    private Map<String,List<WordInfo>> getListMap(List<WordInfo> inputList) {
+    private Map<String, List<WordInfo>> getListMap(List<WordInfo> inputList) {
         Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo input :  inputList){
+        for (WordInfo input : inputList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getWord())){
+            if (!map.containsKey(input.getWord())) {
                 ArrayList arr = new ArrayList<>();
                 arr.add(input);
                 map.put(input.getWord(), arr);
-            }
-
-            else {
+            } else {
                 map.get(input.getWord()).add(input);
             }
         }
